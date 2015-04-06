ae30f667abbc19f2d73b38fd3d7eed034c8be183
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2015-04-06 23:00:24 +0200

    ESP8266 arduino support

--- arduino_functions.h
+++ arduino_functions.h
@@ -6,36 +6,44 @@
 #if (defined(__AVR_ATmega32U4__) || defined(TEENSY20) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega1281__) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega2561__)) && !defined(MAX_RECORDINGS)
   #define MAX_RECORDINGS 512   //on the bigger arduino we have enough SRAM
 #endif
+#if defined(ESP8266)
+  #define MAX_RECORDINGS 512  // ESP8266 has far enough ram
+#endif
 #if !defined(MAX_RECORDINGS)
   #define MAX_RECORDINGS 255   // fallback for undefined Processor.
 #endif
 
-static inline void hw_attachInterrupt(uint32_t pin, void (*callback)(void)) {
+static inline void hw_attachInterrupt(int pin, void (*callback)(void)) {
+  pinMode(pin, INPUT);
   attachInterrupt(pin, callback, CHANGE);
 }
 
-static inline void hw_detachInterrupt(uint32_t pin) {
+static inline void hw_detachInterrupt(int pin) {
   detachInterrupt(pin);
 }
 
-static inline void hw_delayMicroseconds(unsigned long time_to_wait) {
+static inline void hw_delayMicroseconds(uint32_t time_to_wait) {
   //  delayMicroseconds() only works up to 16383 micros
   // https://github.com/pimatic/rfcontroljs/issues/29#issuecomment-85460916
   while(time_to_wait > 16000) {
-    delayMicroseconds(16000);
+    #if defined(ESP8266)
+      delay(16)
+    #else
+      delayMicroseconds(16000);
+    #endif
     time_to_wait -= 16000;
   }
   delayMicroseconds(time_to_wait);
 }
 
-static inline void hw_pinMode(uint8_t pin, uint8_t mode) {
+static inline void hw_pinMode(int pin, int mode) {
   pinMode(pin, mode);
 }
 
-static inline void hw_digitalWrite(uint8_t pin, uint8_t value) {
+static inline void hw_digitalWrite(int pin, int value) {
   digitalWrite(pin, value);
 }
 
-static inline unsigned long hw_micros() {
+static inline uint32_t hw_micros() {
   return micros();
 }
