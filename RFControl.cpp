da2f91caec24e76551aa3a66bb226d178fcb52b6
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2016-06-18 14:27:51 +0200

    Set minimum package length to 16 (was 32) see pimatic/rfcontroljs#101

--- RFControl.cpp
+++ RFControl.cpp
@@ -171,9 +171,9 @@ void recording(unsigned int duration, int package) {
     data_start[package + 1] = data_end[package] + 1;
     data_end[package + 1] = data_start[package + 1];
 
-    //Received more than 32 timings and start and end are the same footer then enter next state
-    //less than 32 timings -> restart the package.
-    if (data_end[package] - data_start[package] >= 32)
+    //Received more than 16 timings and start and end are the same footer then enter next state
+    //less than 16 timings -> restart the package.
+    if (data_end[package] - data_start[package] >= 16)
     {
       if (state == STATUS_RECORDING_3) {
         state = STATUS_RECORDING_END;
